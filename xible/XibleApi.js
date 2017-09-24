549d1a22919c466d5f9daae76ebfcb38bf449ab3
Feanathiel <Feanathiel@users.noreply.github.com>
2017-09-24 13:57:39 +0200

    Exportable flows

--- xible/XibleApi.js
+++ xible/XibleApi.js
@@ -7,6 +7,24 @@ module.exports = (xibleEditor) => {
         center() {
             xibleEditor.center();
         }
+
+        flowsJson() {
+            var data = {};
+
+            for (var key in xibleEditor.flows) {
+                if (!xibleEditor.flows.hasOwnProperty(key)) {
+                    continue;
+                }
+
+                var flow = xibleEditor.flows[key];
+
+                // This should be done differently, but works for now
+                // We need to check what we want to export.
+                data[key] = JSON.parse(flow.toJson());
+            }
+
+            return JSON.stringify(data, null, 4);
+        }
     }
 
     return XibleApi;
